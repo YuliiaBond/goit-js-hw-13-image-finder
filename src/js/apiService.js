684eac25e7002c7d2f2419b5f52400fa3dfbea75
Yuliia Bondarenko <bondarenko047@gmail.com>
2021-05-29 00:02:27 +0300

    added gallery

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,18 +2,32 @@ export default class ApiService {
 
     constructor() {
         this.searchQuery = '';
+        this.page = 1;
     }
     
     fetchGallery() {
-        // console.log(this);
+        console.log(this);
     const key = '21813787-5b33d57d4a7410a6824d2f569';
     
-    fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=1&per_page=12&key=${key}`)
+    return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${key}`)
         .then(response => response.json())
-        .then(console.log)
+        .then(data => {
+            // console.log(data);
+            this.incrementPage();
+
+            return data.hits;
+        })
         .catch(error => {
             console.log(error);
         });
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
     }
 
     get query() {
--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,33 @@
 import ApiService from './apiService';
+import cardTpl from '../templates/card.hbs';
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
     galleryContainer: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('[data-action="load-more"]'),
 };
 
 const apiService = new ApiService()
-refs.searchForm.addEventListener('submit', onSearch);
 
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 function onSearch(event) {
     event.preventDefault();
     
     apiService.query = event.currentTarget.elements.query.value;
+    apiService.resetPage();
+    apiService.fetchGallery().then(appenCardMarkup);
+}
 
-    apiService.fetchGallery()
+function onLoadMore() {
+    apiService.fetchGallery().then(appenCardMarkup);
+}
+
+function appenCardMarkup(hits) {
+    refs.galleryContainer.insertAdjacentHTML('beforeend', cardTpl(hits));
+}
+
+function clearGalleryContainer() {
+    refs.galleryContainer.insertAdjacentHTML = '';
 }
